29b0c9750a9997264841f96c9c549cce3604161d
余显光 <hepper.yu@igg.com>
2024-10-22 18:10:54 +0800

    add: redis config

--- backup-buz/src/main/java/com/backup/server/config/RedisConfig.java
+++ backup-buz/src/main/java/com/backup/server/config/RedisConfig.java
@@ -2,13 +2,13 @@ package com.backup.server.config;
 
 public interface RedisConfig {
     // 订阅主题
-    String REDIS_SERVER_2_AGENT_TOPIC_PREFIX = "backup:message";
-    String REDIS_AGENT_2_SERVER_TOPIC_PREFIX = "";
+    String REDIS_SERVER_2_AGENT_TOPIC_PREFIX = "backup:2agent:";
+    String REDIS_AGENT_2_SERVER_TOPIC_PREFIX = "backup:2server:";
     String REDIS_BROADCAST_TOPIC = "backup:broadcast";
 
     // KEY
-    String REDIS_AGENT_ONLINE_PREFIX = "";
+    String REDIS_AGENT_ONLINE_PREFIX = "backup:online:";
 
     // EVENT
-    String REDIS_AGENT_ONLINE_EVENT = "";
+    String REDIS_AGENT_ONLINE_EVENT = "backup:event:online";
 }
