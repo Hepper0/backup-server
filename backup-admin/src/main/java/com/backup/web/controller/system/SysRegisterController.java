ba3f4e7a6deff60d42e091d981392c3c4e2db5ea
余显光 <hepper.yu@igg.com>
2024-10-22 09:46:59 +0800

    initialization

---
+++ backup-admin/src/main/java/com/backup/web/controller/system/SysRegisterController.java
@@ -0,0 +1,38 @@
+package com.backup.web.controller.system;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RestController;
+import com.backup.common.core.controller.BaseController;
+import com.backup.common.core.domain.AjaxResult;
+import com.backup.common.core.domain.model.RegisterBody;
+import com.backup.common.utils.StringUtils;
+import com.backup.framework.web.service.SysRegisterService;
+import com.backup.system.service.ISysConfigService;
+
+/**
+ * 注册验证
+ *
+ * @author ruoyi
+ */
+@RestController
+public class SysRegisterController extends BaseController
+{
+    @Autowired
+    private SysRegisterService registerService;
+
+    @Autowired
+    private ISysConfigService configService;
+
+    @PostMapping("/register")
+    public AjaxResult register(@RequestBody RegisterBody user)
+    {
+        if (!("true".equals(configService.selectConfigByKey("sys.account.registerUser"))))
+        {
+            return error("当前系统没有开启注册功能！");
+        }
+        String msg = registerService.register(user);
+        return StringUtils.isEmpty(msg) ? success() : error(msg);
+    }
+}
